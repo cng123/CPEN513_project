665f72bdfb8ecf2dca2374e5b80130d18f3cc330
Winnie-Gong <winnie_gong961026@hotmail.com>
2021-04-24 06:37:53 -0700

    added crossover

--- genetic_placement.py
+++ genetic_placement.py
@@ -13,9 +13,14 @@ class gene():
 		self.y = y
 
 class GeneticPlacement():
-	def __init__(benchmark):
+	def __init__(benchmark, crossover_rate, inversion_rate, mutation_rate):
 		self.ny, self.nx, self.total_cell_num, self.nets = preprocess("benchmarks/"+benchmark+".txt")
-		self.grid = np.full((ny,nx), -1)
+		self.crossover_rate = crossover_rate
+		self.inversion_rate = inversion_rate
+		self.mutation_rate = mutation_rate
+		#The mutation rate as given by this parameter can vary from 0 to 10% in steps of
+		# 0.5%. the inversion rate can vary from 0 to 100% in steps of 5%. and the crossover rate can vary from 20% to loo%,
+		# in steps of 4%
 
 	def init_population(population_size):
 		self.population = []
@@ -26,13 +31,64 @@ class GeneticPlacement():
 			rand_positions = random.sample(range(self.ny*self.nx), self.total_cell_num)
 			# decode pos into x, y coordinates, and update on grid
 			for cell_num, pos in enumerate(rand_positions):
-				new_gene = gene(cell_num, cell_num)
+				new_gene = gene(cell_num, pos)
 				new_gene.update_location(floor(n/nx), n%nx)
 				self.grid[floor(n/nx)][n%nx] = cell_num
 				chromosome.append(new_gene)
 			self.population.append(chromosome)
 
-	def crossover():
+	def crossover(p1, p2):
+		cut_i = random.randint(0, self.total_cell_num-1)
+		offspring = [None]*self.total_cell_num
+		if self.crossover_type == "order":
+			offspring[:cut_i] = p1[:cut_i]
+			finalized_genes = set()
+			for g_o in offspring[:cut_i]:
+				finalized_genes.add(g_o.cell_num)
+			i = cut_i+1
+			for g_2 in p2:
+				if g_2.cell_num not in finalized_genes:
+					offspring[:i] = g_2
+					i += 1
+		elif self.crossover_type == "PMT":
+			offspring[cut_i:] = p2[cut_i:]
+			for i in range(cut_i,self.cell_num):
+				if p1[i].cell_num == p2[i].cell_num:
+					continue
+				for j in range(self.cell_num):
+					if p1[j].cell_num == p2[i].cell_num:
+						temp = p1[j]
+						p1[j] = p1[i]
+						p1[i] = p1[j]
+			offspring[:cut_i] = p1[:cut_i]
+		elif self.crossover_type == "cycle":
+			p_src = p1
+			p_ref = p2
+			while None in offspring:
+				visited = set()
+				next_i = 0
+				for i,gene in enumerate(offspring):
+					if gene is None:
+						next_i = i
+						break
+				while True:
+					offspring[next_i] = p_src[next_i]
+					visited.add(p_src[next_i])
+					for i,gene in p_src:
+						if gene.cell_num == p_ref[next_i].cell_num:
+							next_i = i
+							break
+					if p_src[next_i] in visited:
+						break
+
+
+				if p_src is p1:
+					p_src = p2
+					p_ref = p1
+				else:
+					p_src = p1
+					p_ref = p2
+
 
 	def mutation(chromosome):
 		i1, i2 = random.sample(range(self.total_cell_num), 2)
